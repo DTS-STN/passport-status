68730d66b927487887f9554725a72a3ae97ec9c5
Shaun Laughland <22419216+s-laugh@users.noreply.github.com>
2022-09-20 11:17:53 -0400

    add missing file

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,15 @@
 import '../styles/globals.css'
 import Layout from '../components/Layout'
+import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps }) {
-  /* istanbul ignore next */
-  if (Component.getLayout) {
-    return Component.getLayout(<Component {...pageProps} />)
-  }
+  let router = useRouter()
+  router ??= {}
+  let langToggleLink = (pageProps.locale === 'en' ? '/fr' : '') + router.asPath
 
   /* istanbul ignore next */
   return (
-    <Layout locale={pageProps.locale} langToggleLink={pageProps.langToggleLink}>
+    <Layout locale={pageProps.locale} langToggleLink={langToggleLink}>
       <Component {...pageProps} />
     </Layout>
   )
