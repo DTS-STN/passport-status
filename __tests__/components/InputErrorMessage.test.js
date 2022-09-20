cf45dd1dd343608c2352202ebed6610dc388b38a
Shaun Laughland <22419216+s-laugh@users.noreply.github.com>
2022-09-20 16:39:56 -0400

    fix broken test

--- __tests__/components/InputErrorMessage.test.js
+++ __tests__/components/InputErrorMessage.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import { axe, toHaveNoViolations } from 'jest-axe'
-import { InputErrorMessage } from '../../components/InputErrorMessage'
+import InputErrorMessage from '../../components/InputErrorMessage'
 
 expect.extend(toHaveNoViolations)
 
