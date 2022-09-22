e758099f057f2fb481367e6f1609e3c82c151cd9
Shaun Laughland <22419216+s-laugh@users.noreply.github.com>
2022-09-22 15:08:07 -0400

    cleanup test

--- __tests__/api/checkStatus.test.js
+++ __tests__/api/checkStatus.test.js
@@ -3,10 +3,11 @@ import { createMocks } from 'node-mocks-http'
 
 describe('api/checkStatus', () => {
   it('returns a result', async () => {
-    const { req, res } = createMocks({ method: 'POST' })
+    const { req, res } = createMocks({
+      method: 'POST',
+      body: { givenName: 'badName' },
+    })
     await handler(req, res)
-    expect(res._getStatusCode()).toBe(200)
-    const result = res._getJSONData()
-    expect(result).toBeTruthy()
+    expect(res._getStatusCode()).toBe(404)
   })
 })
