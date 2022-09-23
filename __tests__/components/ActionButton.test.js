a540480cca4ce6d193293d858a4bc3b45c73c9cf
Kris Charbonneau <char0799@algonquinlive.com>
2022-09-23 12:13:00 -0400

    fixed test

--- __tests__/components/ActionButton.test.js
+++ __tests__/components/ActionButton.test.js
@@ -17,7 +17,7 @@ describe('ActionButton', () => {
     render(<ActionButton text="text" style="primary" />)
     const sut = screen.getByText('text')
     expect(sut).toHaveClass(
-      'border-blue-deep bg-blue-dark text-basic-white hover:bg-blue-normal'
+      'inline-block text-center align-middle rounded border py-2.5 px-3.5 focus:ring-2 focus:ring-offset-2border-blue-deep bg-blue-dark text-basic-white hover:bg-blue-normal'
     )
   })
 
