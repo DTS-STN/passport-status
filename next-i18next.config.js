391ce58406ea96486541ce4e6a4b0dd00739a6a5
Sébastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2022-10-18 10:59:13 -0300

    Add next-i18next config appendNamespaceToMissingKey

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,12 @@
+// @ts-check
+
+/**
+ * @type {import('next-i18next').UserConfig}
+ **/
 module.exports = {
   i18n: {
     locales: ['default', 'en', 'fr'],
     defaultLocale: 'default',
   },
+  appendNamespaceToMissingKey: true,
 }
