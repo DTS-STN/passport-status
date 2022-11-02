bd04608869e635364e0a5d38610d3693b7c4bb93
Kris Charbonneau <char0799@algonquinlive.com>
2022-11-02 09:59:09 -0400

    Update OpenTelemetry tracing to sned metrics to Dynatrace

--- tracing.js
+++ tracing.js
@@ -1,16 +1,51 @@
-// Require dependencies
-const opentelemetry = require('@opentelemetry/sdk-node')
-const {
-  getNodeAutoInstrumentations,
-} = require('@opentelemetry/auto-instrumentations-node')
+'use strict'
+
 const { diag, DiagConsoleLogger, DiagLogLevel } = require('@opentelemetry/api')
+const {
+  configureDynatraceMetricExport,
+} = require('@dynatrace/opentelemetry-exporter-metrics')
+const { MeterProvider } = require('@opentelemetry/sdk-metrics')
+const { Resource } = require('@opentelemetry/resources')
 
-// For troubleshooting, set the log level to DiagLogLevel.DEBUG
-diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO)
+// optional: set up logging for OpenTelemetry
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.ALL)
 
-const sdk = new opentelemetry.NodeSDK({
-  traceExporter: new opentelemetry.tracing.ConsoleSpanExporter(),
-  instrumentations: [getNodeAutoInstrumentations()],
+// Configure a MeterProvider
+const provider = new MeterProvider({
+  resource: new Resource({
+    'service.name': 'opentelemetry-metrics-dynatrace-passport',
+  }),
 })
 
-sdk.start()
+const reader = configureDynatraceMetricExport(
+  // exporter configuration
+  {
+    // export directly to the Dynatrace server
+    url: process.env.NEXT_PUBLIC_DYNATRACE_INGEST_URL,
+    apiToken: process.env.DYNATRACE_API_KEY,
+  },
+  // metric reader configuration
+  {
+    exportIntervalMillis: 5000,
+  }
+)
+
+provider.addMetricReader(reader)
+
+const meter = provider.getMeter('opentelemetry-metrics-dynatrace-passport')
+
+// Your SDK should be set up correctly now. You can create instruments...
+const requestCounter = meter.createCounter('requests', {
+  description: 'Request Counter',
+})
+
+// ...set up attributes...
+const attributes = {
+  pid: process.pid.toString(),
+  environment: 'staging',
+}
+
+// ... and start recording metrics:
+setInterval(() => {
+  requestCounter.add(Math.round(Math.random() * 1000), attributes)
+}, 1000)
