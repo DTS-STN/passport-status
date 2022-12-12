73b43e76df5ec50df6b91eaa6045d065e2a875b4
Maxim Lam <maxim.lam@hrsdc-rhdcc.gc.ca>
2022-12-12 15:50:48 -0500

    Adjusted styles to match Design System (cursor, hover and focus color)

--- components/Collapse.tsx
+++ components/Collapse.tsx
@@ -8,7 +8,9 @@ export interface CollapseProps {
 const Collapse: FC<CollapseProps> = ({ title, children }) => {
   return (
     <details className="max-w-prose border p-3 mb-3 rounded">
-      <summary className="text-blue-light">{title}</summary>
+      <summary className="text-blue-light hover:underline cursor-pointer focus:text-link-selected hover:text-link-selected">
+        {title}
+      </summary>
       {children}
     </details>
   )
