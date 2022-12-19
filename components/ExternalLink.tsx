1722705b16a43502bcb8c6227fe5f551de6688b6
Kris Charbonneau <char0799@algonquinlive.com>
2022-12-19 11:00:48 -0500

    Update external link component and css

--- components/ExternalLink.tsx
+++ components/ExternalLink.tsx
@@ -9,30 +9,9 @@ export interface ExternalLinkProps {
 export const ExternalLink: FC<ExternalLinkProps> = ({ children, href }) => {
   const { t } = useTranslation(['common'])
   return (
-    <a
-      className="inline-flex items-center"
-      href={href}
-      target="_blank"
-      rel="noopener noreferrer"
-    >
+    <a href={href} target="_blank" rel="noopener noreferrer">
       {children}
-      <span className="sr-only">{t('opens-in-new-tab')}</span>
-      <svg
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M15.6396 7.02527H12.0181V5.02527H19.0181V12.0253H17.0181V8.47528L12.1042 13.3892L10.6899 11.975L15.6396 7.02527Z"
-          fill="currentColor"
-        />
-        <path
-          d="M10.9819 6.97473H4.98193V18.9747H16.9819V12.9747H14.9819V16.9747H6.98193V8.97473H10.9819V6.97473Z"
-          fill="currentColor"
-        />
-      </svg>
+      <span className="sr-only">({t('opens-in-new-tab')})</span>
     </a>
   )
 }
