28a0540d26e690ca859722762d1427d19e775b08
Sébastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2023-01-04 12:52:39 -0400

    Convert Header tests to TS

--- __tests__/components/Header.test.tsx
+++ __tests__/components/Header.test.tsx
@@ -1,23 +1,8 @@
 import { render, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 import { axe, toHaveNoViolations } from 'jest-axe'
-import Header from '../../components/Header'
-
 import { useRouter } from 'next/router'
-
-// mocks useRouter to be able to use component' router.asPath
-jest.mock('next/router', () => ({
-  useRouter: jest.fn(),
-}))
-
-// the code below is to avoid the following error:
-//    "An update to Link inside a test was not wrapped in act(...)"
-jest.mock('next/link', () => ({
-  __esModule: true,
-  default: ({ children, href }) => (
-    <children.type {...children.props} href={href} />
-  ),
-}))
+import Header from '../../components/Header'
 
 const defaultRouterObj = {
   pathname: '/',
@@ -25,26 +10,35 @@ const defaultRouterObj = {
   locale: 'en',
 }
 
+// mocks useRouter to be able to use component' router.asPath
+jest.mock('next/router', () => ({
+  useRouter: jest.fn(() => defaultRouterObj),
+}))
+
 expect.extend(toHaveNoViolations)
 
 describe('Header', () => {
-  useRouter.mockImplementation(() => defaultRouterObj)
-
   it('renders Header in English', () => {
-    render(<Header />)
+    render(<Header gocLink="testGocLink" skipToMainText="testSkipToMainText" />)
     const HeaderLang = screen.getByText('Français')
     expect(HeaderLang).toBeInTheDocument()
   })
 
   it('renders Header in French', () => {
-    useRouter.mockImplementation(() => ({ ...defaultRouterObj, locale: 'fr' }))
-    render(<Header />)
+    const useRouterMock = useRouter as jest.Mock
+    useRouterMock.mockImplementationOnce(() => ({
+      ...defaultRouterObj,
+      locale: 'fr',
+    }))
+    render(<Header gocLink="testGocLink" skipToMainText="testSkipToMainText" />)
     const HeaderLang = screen.getByText('English')
     expect(HeaderLang).toBeInTheDocument()
   })
 
   it('has no a11y violations', async () => {
-    const { container } = render(<Header skipToMainText="skiptext" />)
+    const { container } = render(
+      <Header gocLink="testGocLink" skipToMainText="testSkipToMainText" />
+    )
     const results = await axe(container)
     expect(results).toHaveNoViolations()
   })
