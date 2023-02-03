3c50d0a8e57bf2659b7c481b0265f3621b2f1fee
Kris Charbonneau <char0799@algonquinlive.com>
2023-02-03 12:25:23 -0500

    actually add the conditional...

--- components/InputLabel.tsx
+++ components/InputLabel.tsx
@@ -17,9 +17,11 @@ const InputLabel: FC<InputLabelProps> = ({
 }) => {
   return (
     <label id={id} htmlFor={htmlFor} className="mb-2 block font-bold">
-      <b className="text-accent-error mr-0.5" aria-hidden="true">
-        *
-      </b>
+      {required && (
+        <b className="text-accent-error mr-0.5" aria-hidden="true">
+          *
+        </b>
+      )}
       {label}
       {required && (
         <strong className="text-accent-error">&nbsp;{textRequired}</strong>
