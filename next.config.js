efd1a838a984c2cfabde5ce5dee543194a97b4f2
Sébastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2023-02-06 15:33:06 -0400

    Add Adobe Analytics

--- next.config.js
+++ next.config.js
@@ -42,7 +42,7 @@ const securityHeaders = [
   {
     key: 'Content-Security-Policy',
     value:
-      "default-src 'self'; base-uri 'self'; frame-ancestors 'self'; form-action 'self'; object-src 'none'; script-src-elem 'self'; script-src 'self' 'unsafe-eval'; connect-src 'self'; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https:",
+      "default-src 'self'; base-uri 'self'; frame-ancestors 'self'; form-action 'self'; object-src 'none'; script-src 'self' 'unsafe-eval' 'unsafe-inline' https://code.jquery.com https://*.demdex.net https://cm.everesttech.net https://assets.adobedtm.com https://www.youtube.com; connect-src 'self' https://*.demdex.net https://cm.everesttech.net https://assets.adobedtm.com https://*.omtrdc.net; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com; img-src 'self' data: https://*.demdex.net https://cm.everesttech.net https://assets.adobedtm.com https://*.omtrdc.net; frame-src 'self' https://*.demdex.net;",
   },
 ]
 
@@ -55,6 +55,8 @@ const nextConfig = {
     LOGGING_LEVEL: process.env.LOGGING_LEVEL ?? 'info',
   },
   publicRuntimeConfig: {
+    adobeAnalyticsScriptSrc:
+      process.env.NEXT_PUBLIC_ADOBE_ANALYTICS_SCRIPT_SRC ?? undefined,
     appBaseUri: process.env.NEXT_PUBLIC_APP_BASE_URI ?? '',
     environment: process.env.NEXT_PUBLIC_ENVIRONMENT ?? '',
   },
