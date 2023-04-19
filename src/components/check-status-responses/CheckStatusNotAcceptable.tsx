02c340880003ae564363ffdce2ff79c11c7fe8a2
Stefan O'Connell <stefan.oconnell@hrsdc-rhdcc.gc.ca>
2023-04-19 10:12:38 -0300

    Result not acceptable changes

--- src/components/check-status-responses/CheckStatusNotAcceptable.tsx
+++ src/components/check-status-responses/CheckStatusNotAcceptable.tsx
@@ -2,19 +2,23 @@ import { FC } from 'react'
 
 import { Trans, useTranslation } from 'next-i18next'
 
+import ExternalLink from '../ExternalLink'
+
 export const CheckStatusNotAcceptable: FC<{}> = () => {
   const { t } = useTranslation(['status', 'common'])
   return (
     <>
-      <h2 data-testid="not-acceptable" className="h2">
+      <h1 data-testid="not-acceptable" className="h1">
         {t('not-acceptable.cannot-process')}
-      </h2>
+      </h1>
       <p>{t('not-acceptable.explanation')}</p>
       <p>
         <Trans
-          i18nKey="not-acceptable.call-us"
+          i18nKey="not-acceptable.contact-us"
           ns="status"
-          tOptions={{ phoneNumber: t('common:phone-number') }}
+          components={{
+            Link: <ExternalLink href={t('common.contact-us-link')} />,
+          }}
         />
       </p>
     </>
