04a5a92f26b8a3a18c6c4b07171377d542dfd576
Stefan O'Connell <stefan.oconnell@hrsdc-rhdcc.gc.ca>
2023-04-19 15:58:16 -0300

    Fedex change

--- src/components/check-status-responses/CheckStatusShippingFedex.tsx
+++ src/components/check-status-responses/CheckStatusShippingFedex.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react'
 
 import { Trans, useTranslation } from 'next-i18next'
 
+import AlertSection from '../AlertSection'
 import ExternalLink from '../ExternalLink'
 
 export interface CheckStatusShippingFedexProps {
@@ -11,35 +12,46 @@ export interface CheckStatusShippingFedexProps {
 export const CheckStatusShippingFedex: FC<CheckStatusShippingFedexProps> = ({
   trackingNumber,
 }) => {
-  const { t } = useTranslation('status')
+  const { t } = useTranslation(['status', 'common'])
   return (
     <>
-      <h2 data-testid="shipped-fedex" className="h2">
+      <h1 data-testid="shipped-fedex" className="h1">
         {t('shipped-fedex.header')}
-      </h2>
-      <p>
-        {t('shipped-fedex.mailing')}
-        {trackingNumber && (
-          <>
-            {' '}
-            <Trans
-              i18nKey="status-check-tracking.number"
-              ns="status"
-              tOptions={{ trackingNumber }}
-            />
-          </>
-        )}
-      </p>
-      <p>
-        <ExternalLink
-          href={t('status-check-tracking.link.fedex', {
-            trackingNumber,
-          })}
-        >
-          {t('status-check-tracking.can-track')}
-        </ExternalLink>
-      </p>
+      </h1>
+      <AlertSection type="success">
+        <h2 data-testid="shipped-fedex-mailing" className="h2">
+          {t('shipped-fedex.mailing')}
+          {trackingNumber && (
+            <>
+              {' '}
+              <Trans
+                i18nKey="status-check-tracking.number"
+                ns="status"
+                tOptions={{ trackingNumber }}
+              />
+            </>
+          )}
+        </h2>
+        <p>
+          <ExternalLink
+            href={t('status-check-tracking.link.fedex', {
+              trackingNumber,
+            })}
+          >
+            {t('status-check-tracking.can-track')}
+          </ExternalLink>
+        </p>
+      </AlertSection>
       <p className="mt-6">{t('shipped-fedex.supporting-documents')}</p>
+      <p className="mt-6">
+        <Trans
+          i18nKey="shipped-fedex.contact-us"
+          ns="status"
+          components={{
+            Link: <ExternalLink href={t('common.contact-us-link')} />,
+          }}
+        />
+      </p>
     </>
   )
 }
