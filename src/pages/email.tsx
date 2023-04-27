5e0ab2f74185bfd19ff8c4e475d7e8c7fd870692
Stefan O'Connell <stefan.oconnell@hrsdc-rhdcc.gc.ca>
2023-04-27 17:13:03 -0300

    Email request received changes

--- src/pages/email.tsx
+++ src/pages/email.tsx
@@ -25,6 +25,7 @@ import ErrorSummary, {
   getErrorSummaryItems,
   goToErrorSummary,
 } from '../components/ErrorSummary'
+import ExternalLink from '../components/ExternalLink'
 import IdleTimeout from '../components/IdleTimeout'
 import InputField from '../components/InputField'
 import Layout from '../components/Layout'
@@ -148,19 +149,40 @@ const Email: FC = () => {
         additionalMetaTags={[getDCTermsTitle(en('header'), fr('header'))]}
       />
       <IdleTimeout />
-      <h1 ref={headingRef} className="h1" tabIndex={-1}>
-        {t('header')}
-      </h1>
 
       {isEmailEsrfSuccess ? (
         <div id="response-result">
-          <h2 className="h2">{t('email-confirmation-msg.request-received')}</h2>
-          <p>{t('email-confirmation-msg.if-exists')}</p>
+          <h1 className="h1">{t('email-confirmation-msg.request-received')}</h1>
+          <p>
+            <Trans i18nKey="email-confirmation-msg.if-exists" ns="email" />
+          </p>
+          <p>{t('email-confirmation-msg.may-not-receive')}</p>
+          <ul className="mb-5 list-disc space-y-2 pl-10">
+            <li>
+              <Trans
+                i18nKey="email-confirmation-msg.may-not-receive-list.item-1"
+                ns="email"
+              />
+            </li>
+            <li>{t('email-confirmation-msg.may-not-receive-list.item-2')}</li>
+          </ul>
+          <p>{t('email-confirmation-msg.dont-receive')}</p>
+          <ul className="mb-5 list-disc space-y-2 pl-10">
+            <li>
+              <Trans
+                i18nKey="email-confirmation-msg.dont-receive-list.item-1"
+                ns="email"
+              />
+            </li>
+            <li>{t('email-confirmation-msg.dont-receive-list.item-2')}</li>
+          </ul>
           <p>
             <Trans
-              i18nKey="email-confirmation-msg.please-contact"
+              i18nKey={'email-confirmation-msg.please-contact'}
               ns="email"
-              tOptions={{ phoneNumber: t('common:phone-number') }}
+              components={{
+                Link: <ExternalLink href={t('common:contact-us-link')} />,
+              }}
             />
           </p>
           <div className="my-8">
@@ -174,107 +196,112 @@ const Email: FC = () => {
           </div>
         </div>
       ) : (
-        <form onSubmit={handleFormikSubmit} id="form-email-esrf">
-          <p>
-            <Trans i18nKey="header-messages.matches" ns="email" />
-          </p>
-          <ul className="mb-5 list-disc space-y-2 pl-10">
-            <li>{t('header-messages.list.item-1')}</li>
-            <li>{t('header-messages.list.item-2')}</li>
-            <li>
-              <Trans i18nKey="header-messages.list.item-3" ns="email" />
-            </li>
-            <li>{t('header-messages.list.item-4')}</li>
-          </ul>
-          <AlertSection type="info">
-            <p className="h3">{t('header-messages.child-applications')}</p>
+        <>
+          <h1 ref={headingRef} className="h1" tabIndex={-1}>
+            {t('header')}
+          </h1>
+          <form onSubmit={handleFormikSubmit} id="form-email-esrf">
             <p>
-              <Trans
-                i18nKey="header-messages.for-child-application"
-                ns="email"
-              />
+              <Trans i18nKey="header-messages.matches" ns="email" />
             </p>
-          </AlertSection>
+            <ul className="mb-5 list-disc space-y-2 pl-10">
+              <li>{t('header-messages.list.item-1')}</li>
+              <li>{t('header-messages.list.item-2')}</li>
+              <li>
+                <Trans i18nKey="header-messages.list.item-3" ns="email" />
+              </li>
+              <li>{t('header-messages.list.item-4')}</li>
+            </ul>
+            <AlertSection type="info">
+              <p className="h3">{t('header-messages.child-applications')}</p>
+              <p>
+                <Trans
+                  i18nKey="header-messages.for-child-application"
+                  ns="email"
+                />
+              </p>
+            </AlertSection>
 
-          {errorSummaryItems.length > 0 && (
-            <ErrorSummary
-              id="error-summary-email-esrf"
-              summary={t('common:found-errors', {
-                count: errorSummaryItems.length,
-              })}
-              errors={errorSummaryItems}
-            />
-          )}
+            {errorSummaryItems.length > 0 && (
+              <ErrorSummary
+                id="error-summary-email-esrf"
+                summary={t('common:found-errors', {
+                  count: errorSummaryItems.length,
+                })}
+                errors={errorSummaryItems}
+              />
+            )}
 
-          <InputField
-            id="email"
-            name="email"
-            type="email"
-            label={t('email.label')}
-            onChange={handleFormikChange}
-            value={formikValues.email}
-            errorMessage={formikErrors.email && t(formikErrors.email)}
-            textRequired={t('common:required')}
-            required
-            helpMessage={
-              <>
-                <p>{t('help-message.email')} </p>
-                <p>
-                  <Trans
-                    i18nKey="help-message.for-child-application"
-                    ns="email"
-                  />
-                </p>
-              </>
-            }
-          />
-          <InputField
-            id="givenName"
-            name="givenName"
-            label={t('given-name.label')}
-            onChange={handleFormikChange}
-            value={formikValues.givenName}
-            errorMessage={formikErrors.givenName && t(formikErrors.givenName)}
-            textRequired={t('common:required')}
-            required
-          />
-          <InputField
-            id="surname"
-            name="surname"
-            label={t('surname.label')}
-            onChange={handleFormikChange}
-            value={formikValues.surname}
-            errorMessage={formikErrors.surname && t(formikErrors.surname)}
-            textRequired={t('common:required')}
-            required
-          />
-          <DateSelectField
-            id="dateOfBirth"
-            label={t('date-of-birth.label')}
-            onChange={handleOnDateOfBirthChange}
-            value={formikValues.dateOfBirth}
-            errorMessage={
-              formikErrors.dateOfBirth && t(formikErrors.dateOfBirth)
-            }
-            textRequired={t('common:required')}
-            required
-          />
-          <div className="mt-8 flex flex-wrap gap-2">
-            <ActionButton
-              id="btn-submit"
-              disabled={isEmailEsrfLoading}
-              type="submit"
-              text={t('email-esrf')}
-              style="primary"
+            <InputField
+              id="email"
+              name="email"
+              type="email"
+              label={t('email.label')}
+              onChange={handleFormikChange}
+              value={formikValues.email}
+              errorMessage={formikErrors.email && t(formikErrors.email)}
+              textRequired={t('common:required')}
+              required
+              helpMessage={
+                <>
+                  <p>{t('help-message.email')}</p>
+                  <p>
+                    <Trans
+                      i18nKey="help-message.for-child-application"
+                      ns="email"
+                    />
+                  </p>
+                </>
+              }
             />
-            <ActionButton
-              id="btn-cancel"
-              disabled={isEmailEsrfLoading}
-              text={t('common:modal-go-back.cancel-button')}
-              onClick={handleOnCancelClick}
+            <InputField
+              id="givenName"
+              name="givenName"
+              label={t('given-name.label')}
+              onChange={handleFormikChange}
+              value={formikValues.givenName}
+              errorMessage={formikErrors.givenName && t(formikErrors.givenName)}
+              textRequired={t('common:required')}
+              required
             />
-          </div>
-        </form>
+            <InputField
+              id="surname"
+              name="surname"
+              label={t('surname.label')}
+              onChange={handleFormikChange}
+              value={formikValues.surname}
+              errorMessage={formikErrors.surname && t(formikErrors.surname)}
+              textRequired={t('common:required')}
+              required
+            />
+            <DateSelectField
+              id="dateOfBirth"
+              label={t('date-of-birth.label')}
+              onChange={handleOnDateOfBirthChange}
+              value={formikValues.dateOfBirth}
+              errorMessage={
+                formikErrors.dateOfBirth && t(formikErrors.dateOfBirth)
+              }
+              textRequired={t('common:required')}
+              required
+            />
+            <div className="mt-8 flex flex-wrap gap-2">
+              <ActionButton
+                id="btn-submit"
+                disabled={isEmailEsrfLoading}
+                type="submit"
+                text={t('email-esrf')}
+                style="primary"
+              />
+              <ActionButton
+                id="btn-cancel"
+                disabled={isEmailEsrfLoading}
+                text={t('common:modal-go-back.cancel-button')}
+                onClick={handleOnCancelClick}
+              />
+            </div>
+          </form>
+        </>
       )}
       <Modal
         open={modalOpen}
