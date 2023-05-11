ea6a3799efc40d6bc13e400d7a7eb7c7025c50b5
Sébastien Comeau <sebastien.comeau@hrsdc-rhdcc.gc.ca>
2023-05-11 13:54:01 -0300

    add data-gc-analytics-exempt link attribute

--- src/components/ExternalLink.tsx
+++ src/components/ExternalLink.tsx
@@ -1,15 +1,29 @@
-import { FC, PropsWithChildren } from 'react'
+import {
+  AnchorHTMLAttributes,
+  DetailedHTMLProps,
+  FC,
+  PropsWithChildren,
+} from 'react'
 
 import { useTranslation } from 'next-i18next'
 
-export interface ExternalLinkProps extends PropsWithChildren {
+export interface ExternalLinkProps
+  extends DetailedHTMLProps<
+      AnchorHTMLAttributes<HTMLAnchorElement>,
+      HTMLAnchorElement
+    >,
+    PropsWithChildren {
   href: string
 }
 
-export const ExternalLink: FC<ExternalLinkProps> = ({ children, href }) => {
+export const ExternalLink: FC<ExternalLinkProps> = ({
+  children,
+  href,
+  ...rest
+}) => {
   const { t } = useTranslation(['common'])
   return (
-    <a href={href} target="_blank" rel="noopener noreferrer">
+    <a {...rest} href={href} target="_blank" rel="noopener noreferrer">
       {children}
       <span className="sr-only">{t('opens-in-new-tab')}</span>
     </a>
