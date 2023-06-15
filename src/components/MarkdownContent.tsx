a85c572dd3a3a02b2b470a32701472c8dee3a3db
Stefan O'Connell <stefan.oconnell@hrsdc-rhdcc.gc.ca>
2023-06-15 15:59:00 -0300

    Fixed headers and links in markdown

--- src/components/MarkdownContent.tsx
+++ src/components/MarkdownContent.tsx
@@ -1,19 +1,27 @@
-import { FC, useEffect } from 'react'
+import { FC } from 'react'
 
-import { useRemark } from 'react-remark'
+import ReactMarkdown from 'react-markdown'
+
+import ExternalLink from './ExternalLink'
 
 export type MarkdownContentProps = {
   markdown: string
 }
 
-const MarkdownContent: FC<MarkdownContentProps> = ({ markdown }) => {
-  const [reactContent, setMarkdownSource] = useRemark()
-
-  useEffect(() => {
-    setMarkdownSource(markdown)
-  }, [markdown])
-
-  return reactContent
-}
+const MarkdownContent: FC<MarkdownContentProps> = ({ markdown }) => (
+  <ReactMarkdown
+    components={{
+      h1: ({ node, ...props }) => <h1 {...props} className="h1 mt-0" />,
+      h2: ({ node, ...props }) => <h2 {...props} className="h2 mt-0" />,
+      h3: ({ node, ...props }) => <h3 {...props} className="h3 mt-0" />,
+      h4: ({ node, ...props }) => <h4 {...props} className="h4 mt-0" />,
+      a: ({ children, href }) => (
+        <ExternalLink href={href ?? ''}>{children}</ExternalLink>
+      ),
+    }}
+  >
+    {markdown}
+  </ReactMarkdown>
+)
 
 export default MarkdownContent
