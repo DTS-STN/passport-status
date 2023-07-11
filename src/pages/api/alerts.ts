2f971343d57e9946ea77658c65a90ab3235dd527
Stefan O'Connell <stefan.oconnell@hrsdc-rhdcc.gc.ca>
2023-07-11 13:41:31 -0300

    Fixed lets and error return

--- src/pages/api/alerts.ts
+++ src/pages/api/alerts.ts
@@ -9,35 +9,35 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Alert[] | string>
 ) {
-  if (!process.env.ALERT_JSON_URI) {
-    logger.error('ALERT_JSON_URI must not be undefined, null or empty')
-    throw Error(
-      'process.env.ALERT_JSON_URI must not be undefined, null or empty'
-    )
-  }
-
-  if (req.method !== 'GET') {
-    res.status(405).send(`Invalid request method ${req.method}`)
-    return
-  }
-
-  const query = req.query
-  const { page } = query
-
-  let now = new Date()
-
   try {
+    if (!process.env.ALERT_JSON_URI) {
+      logger.error('ALERT_JSON_URI must not be undefined, null or empty')
+      throw Error(
+        'process.env.ALERT_JSON_URI must not be undefined, null or empty'
+      )
+    }
+
+    if (req.method !== 'GET') {
+      res.status(405).send(`Invalid request method ${req.method}`)
+      return
+    }
+
+    const query = req.query
+    const { page } = query
+
+    let now = new Date()
+
     const alertJson = await fetch(process.env.ALERT_JSON_URI, {
       headers: { 'Cache-Control': 'max-age=600' },
     })
     const alertData: AlertJsonResponse = await alertJson.json()
 
-    let validAlerts = alertData?.jsonAlerts.filter(
+    const validAlerts = alertData?.jsonAlerts.filter(
       (alert) =>
         new Date(alert.validFrom) <= now && new Date(alert.validTo) >= now
     )
 
-    let pageAlerts = page
+    const pageAlerts = page
       ? validAlerts.filter((alert) =>
           alert.pages?.find((alertPage) => alertPage === page)
         )
@@ -45,7 +45,7 @@ export default async function handler(
           (alert) => alert.pages === undefined || alert.pages?.length === 0
         )
 
-    let alerts: Alert[] = pageAlerts.map((alert) => ({
+    const alerts: Alert[] = pageAlerts.map((alert) => ({
       uid: alert.uid,
       textEn: alert.textEn,
       textFr: alert.textFr,
@@ -56,6 +56,6 @@ export default async function handler(
   } catch (error) {
     // If there's a problem with the alerts, we return 500 but with an empty list
     logger.error(error, 'Failed to fetch alerts')
-    res.status(500).json([])
+    return res.status(500).json([])
   }
 }
