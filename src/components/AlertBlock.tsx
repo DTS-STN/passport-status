f4830c7fde3a783d60f77988e5cbf5a8625ff596
Stefan O'Connell <stefan.oconnell@hrsdc-rhdcc.gc.ca>
2024-11-14 15:40:53 -0400

    Fix data null check

--- src/components/AlertBlock.tsx
+++ src/components/AlertBlock.tsx
@@ -14,7 +14,7 @@ const AlertBlock = ({ page, className }: AlertBlockProps) => {
   const { data } = useAlerts({ page })
   const { i18n } = useTranslation()
 
-  return data !== undefined && data !== null && data.length > 0 ? (
+  return data && data.length > 0 ? (
     <div className={`${className} pt-4`}>
       {data?.map(({ textEn, textFr, type, uid }) => {
         const markdown = i18n.language === 'fr' ? textFr : textEn
