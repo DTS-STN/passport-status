754752426a7a2cb88fadaad281f4b507e77f3be2
Stefan O'Connell <stefan.oconnell@hrsdc-rhdcc.gc.ca>
2024-12-18 13:35:41 -0400

    a11y 27866: Standardize all header sizes.

--- src/components/ApplicationNameBar.tsx
+++ src/components/ApplicationNameBar.tsx
@@ -9,14 +9,12 @@ const ApplicationNameBar = ({ text, href }: ApplicationNameBarProps) => {
   return (
     <div id="app-bar" className="bg-blue-dark">
       <section className="container mx-auto p-4">
-        <h2>
-          <Link
-            href={href}
-            className="font-body text-lg font-bold text-white underline md:text-[28px]"
-          >
-            {text}
-          </Link>
-        </h2>
+        <Link
+          href={href}
+          className="font-body text-lg font-bold text-white underline md:text-[28px]"
+        >
+          {text}
+        </Link>
       </section>
     </div>
   )
