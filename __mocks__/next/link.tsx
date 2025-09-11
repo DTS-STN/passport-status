06ff04f699eb995f0ef8b0e848ec6313d5bd5ed3
Ken Blanchard <ken.blanchard@hrsdc-rhdcc.gc.ca>
2025-09-11 14:04:14 -0300

    chore(mocks): add mock Link component for testing

---
+++ __mocks__/next/link.tsx
@@ -0,0 +1,11 @@
+import { PropsWithChildren } from 'react'
+
+interface LinkProps extends PropsWithChildren {
+  href: string
+}
+
+const Link = ({ children, href }: LinkProps) => {
+  return <a href={href}>{children}</a>
+}
+
+export default Link
