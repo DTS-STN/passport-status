49ee66edc81b60bb669447638b87189e99e30ee2
Ken Blanchard <ken.blanchard@hrsdc-rhdcc.gc.ca>
2025-09-11 13:15:14 -0300

    chore(deps): update React and TypeScript dependencies; migrate translation imports to next-i18next

--- jest.config.ts
+++ jest.config.ts
@@ -4,18 +4,13 @@ const config: Config = {
   modulePathIgnorePatterns: ['./cypress'],
   collectCoverageFrom: ['src/**/*.{js,jsx,ts,tsx}'],
   moduleNameMapper: {
-    /* Handle CSS imports (with CSS modules)
-      https://jestjs.io/docs/webpack#mocking-css-modules */
+    // Handle CSS imports (with CSS modules) https://jestjs.io/docs/webpack#mocking-css-modules
     '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
-
     // Handle CSS imports (without CSS modules)
     '^.+\\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.js',
-
-    /* Handle image imports
-      https://jestjs.io/docs/webpack#handling-static-assets */
+    // Handle image imports https://jestjs.io/docs/webpack#handling-static-assets
     '^.+\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js',
-
-    /* Handle React markdown not being pre-compiled */
+    // Handle React markdown not being pre-compiled
     'react-markdown':
       '<rootDir>/node_modules/react-markdown/react-markdown.min.js',
   },
@@ -29,9 +24,20 @@ const config: Config = {
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
   testEnvironment: 'jsdom',
   transform: {
-    /* Use babel-jest to transpile tests with the next/babel preset
-      https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object */
-    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+    // Use babel-jest to transpile tests with the next/babel preset
+    '^.+\\.(js|jsx|ts|tsx)$': [
+      'babel-jest',
+      {
+        presets: [
+          [
+            'next/babel',
+            {
+              'preset-react': { runtime: 'automatic' }, // ✅ enable modern JSX
+            },
+          ],
+        ],
+      },
+    ],
   },
   transformIgnorePatterns: [
     '/node_modules/(?!@faker-js/faker)', // Allow @faker-js/faker to be transpiled, v10 uses ESM
